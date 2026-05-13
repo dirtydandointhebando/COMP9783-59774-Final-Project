bb8b9eaa7b362d3906a6518280c337dd7140563c
Aidan <aidandepaul@gmail.com>
2026-05-13 18:19:53 -0400

    Create data folder with placeholder events data

---
+++ nightlife-guestlist-dashboard/src/data/events.js
@@ -0,0 +1,32 @@
+const events = [
+
+  {
+    id: 1,
+    name: "Saturday at Noir",
+    venue: "Club Noir",
+    date: "May 24, 2026",
+    expectedGuests: 48,
+    arrivedGuests: 31
+  },
+
+  {
+    id: 2,
+    name: "Velvet Fridays",
+    venue: "Velvet Room",
+    date: "May 26, 2026",
+    expectedGuests: 72,
+    arrivedGuests: 54
+  },
+
+  {
+    id: 3,
+    name: "Electric Nights",
+    venue: "District Lounge",
+    date: "May 30, 2026",
+    expectedGuests: 38,
+    arrivedGuests: 11
+  }
+
+];
+
+export default events;
